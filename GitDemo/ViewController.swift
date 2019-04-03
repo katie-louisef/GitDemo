094d620209bf55ee8a40ad68cb97152c1172d5c9
Katie Foster (s5004704) <s5004704@mac42937.student.bournemouth.ac.uk>
2019-04-03 11:30:29 +0100

    Initial Commit

---
+++ GitDemo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GitDemo
+//
+//  Created by Katie Foster (s5004704) on 4/3/19.
+//  Copyright © 2019 Katie Foster (s5004704). All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
